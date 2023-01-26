fcfa3ab2ca36f3ddfc3f8b8425304abfb1cd6069
AJota <alum.aortizg.2021@iesalixar.org>
2023-01-26 21:18:34 +0100

    bbdd

--- Proyecto/blog/models.py
+++ Proyecto/blog/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
+class Clientes(models.Model):
+    nombre = models.CharField(max_length=50)
+    direccion = models.CharField(max_length=75)
+    email = models.EmailField(max_length=50)
+    telefono = models.CharField(max_length=9)
+
+class Articulos(models.Model):
+    nombre = models.CharField(max_length=50)
+    seccion = models.CharField(max_length=50)
+    precio = models.CharField(max_length=10)
+
+class Pedidos(models.Model):
+    numero = models.IntegerField()
+    fecha = models.DateField(max_length=15)
+    entregado = models.CharField(max_length=20)
+
+
 # Create your models here.
--- Proyecto/blog/views.py
+++ Proyecto/blog/views.py
@@ -53,5 +53,15 @@ class Persona(object):
         self.nombre = nombre
         self.apellido = apellido
 
+def curso_django(request):
+    fecha_actual = datetime.datetime.now()
+    return render(request, "curso_django.html", {"fecha_actual":fecha_actual})
+
+def curso_python(request):
+    fecha_actual = datetime.datetime.now()
+    return render(request, "curso_python.html", {"fecha_actual":fecha_actual})
+
+
+
 
 # Create your views here.
